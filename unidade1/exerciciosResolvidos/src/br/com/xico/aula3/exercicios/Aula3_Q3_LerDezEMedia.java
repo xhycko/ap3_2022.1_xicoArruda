a08cd520e58d617c8740aefdec016325b0366806
ap3ufersa <ap3ufersa@gmail.com>
2022-08-22 20:08:19 -0300

    Update Aula3_Q3_LerDezEMedia.java

--- unidade1/exerciciosResolvidos/src/br/com/xico/aula3/exercicios/Aula3_Q3_LerDezEMedia.java
+++ unidade1/exerciciosResolvidos/src/br/com/xico/aula3/exercicios/Aula3_Q3_LerDezEMedia.java
@@ -5,28 +5,28 @@ import java.util.Scanner;
 //3. Escreva um algoritmo para ler 10 números e ao final da leitura escrever a soma total dos 10 números lidos.	public static void main(String[] args) {
 public class Aula3_Q3_LerDezEMedia {
 
-	Scanner entradaPadrao = new Scanner(System.in);
-	int soma = 0;
-	float media = 0;
+	public static void main(String[] args) {
 
-	int valores[] = new int[10];
+		Scanner entradaPadrao = new Scanner(System.in);
+		int soma = 0;
+		float media = 0;
 
-	for(
-	int i = 0;i<valores.length;i++)
-	{
-		System.out.println("Digite o valor " + (i + 1) + ":");
-		valores[i] = entradaPadrao.nextInt();
-		soma += valores[i];
+		int valores[] = new int[10];
+
+		for (int i = 0; i < valores.length; i++) {
+			System.out.println("Digite o valor " + (i + 1) + ":");
+			valores[i] = entradaPadrao.nextInt();
+			soma += valores[i];
+		}
+
+		for (int i = 0; i < valores.length; i++)
+			System.out.println("Valor[" + i + "]=" + valores[i]);
+
+		media = soma / valores.length;
+
+		System.out.println("A m�dia �:" + media);
+
+		entradaPadrao.close();
 	}
-
-	for(
-	int i = 0;i<valores.length;i++)System.out.println("Valor["+i+"]="+valores[i]);
-
-	media=soma/valores.length;
-
-	System.out.println("A m�dia �:"+media);
-
-	entradaPadrao.close();
-}
 
 }

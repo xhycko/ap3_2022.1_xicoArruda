271e2d39e28c4b0593a260057ffd2ee0e0d464ed
ap3ufersa <ap3ufersa@gmail.com>
2022-08-26 20:20:09 -0300

    Alteracoes da Aula 4

---
+++ ProjetoEmpregado/ProjetoEmpregado/src/TestaEmpregado.java
@@ -0,0 +1,5 @@
+public class TestaEmpregado {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Aqui testaremos os empregados!");
+    }
+}
--- unidade1/exerciciosResolvidos/src/br/com/xico/aula3/exercicios/Aula3_Q5_MuitasIdades.java
+++ unidade1/exerciciosResolvidos/src/br/com/xico/aula3/exercicios/Aula3_Q5_MuitasIdades.java
@@ -3,8 +3,16 @@ package br.com.xico.aula3.exercicios;
 import java.util.Random;
 
 //3. Faça um programa em Java que leia da entrada padrão a idade de 4096 pessoas e exiba quantas pessoas são maiores de idade.
+/**
+ * Classe de muitas idades
+ * @author Xico
+ * @since 26/08/2022
+ */
 public class Aula3_Q5_MuitasIdades {
-
+	/**
+	 * Método principal do meu sistema.
+	 * @param args Argumentos da linha de comando
+	 */
 	public static void main(String[] args) {
 		int tamanho = 4096;
 		int[] idades = new int[tamanho];
